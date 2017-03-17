0916b9a3bedf77913cee28add217763f01d89d59
Jack X <namanvrm7@gmail.com>
2017-03-17 13:10:41 +0530

    Update M_FredoAndLargeNumbers.py
    Modified and added a few more optimizations to make it faster.
    Made it skip iterations that gave result zero, and added an optimization for repeated inputs.
    
    Passing test cases 1 to 8
    9 to 12 test cases still giving TLE

--- M_FredoAndLargeNumbers.py
+++ M_FredoAndLargeNumbers.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict as ODict
-
 N=int(input())
 arr = input().split()
 d=dict()
@@ -8,26 +6,38 @@ for a in arr:
 		d[a] = d[a]+1
 	else:
 		d[a]=1
-#print(d)
-dd = ODict()
-
-for a in d.keys():
-	dd[a] = d[a]
-
-#print (dd)	
 
 Q=int(input())
+dlookup = dict()
 
 for x in range(0,Q):
-	query = input().split()
+	inp = input()
+	query = inp.split()
 	f = int(query[1])
+	
+	
+	# if(d[arr[len(arr)-1]] < f):
+		# print('0')
+		# continue
+	
+	if(inp in dlookup.keys()):
+		print(dlookup[inp])
+		continue
+	maxKey=max(d,key=d.get)	
+	if(d[maxKey] < f):
+		print('0')
+		dlookup[inp] = '0'
+		continue
+	
+	
 	if (query[0] == '0'):
 		#Type 0 Query
 		#First element whose frequency is atleast f	
 		flag = 0
 		for a in arr:
-			if(dd[a] >= f):
+			if(d[a] >= f):
 				print(a)
+				dlookup[inp]= a
 				flag = 1 
 				break
 		if(flag==0):
@@ -35,9 +45,58 @@ for x in range(0,Q):
 	else:
 		flag = 0
 		for a in arr:
-			if(dd[a] == f):
+			if(d[a] == f):
 				print(a)
+				dlookup[inp] = a
 				flag = 1
 				break
 		if(flag == 0):
 			print('0')
+			
+			
+################# Previous Code ###############
+#### The Code below worked for half the test cases, but showed TLE for others #########
+
+# from collections import OrderedDict as ODict
+
+# N=int(input())
+# arr = input().split()
+# d=dict()
+# for a in arr:
+	# if a in d.keys():
+		# d[a] = d[a]+1
+	# else:
+		# d[a]=1
+##print(d)
+# dd = ODict()
+
+# for a in d.keys():
+	# dd[a] = d[a]
+
+##print (dd)	
+
+# Q=int(input())
+
+# for x in range(0,Q):
+	# query = input().split()
+	# f = int(query[1])
+	# if (query[0] == '0'):
+		##Type 0 Query
+		##First element whose frequency is atleast f	
+		# flag = 0
+		# for a in arr:
+			# if(dd[a] >= f):
+				# print(a)
+				# flag = 1 
+				# break
+		# if(flag==0):
+			# print('0')
+	# else:
+		# flag = 0
+		# for a in arr:
+			# if(dd[a] == f):
+				# print(a)
+				# flag = 1
+				# break
+		# if(flag == 0):
+			# print('0')
