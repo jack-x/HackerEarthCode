cd121ec2d91d06bb327a92bbd089d1c38849dd9d
Jack X <namanvrm7@gmail.com>
2017-05-23 12:07:10 +0530

    Update M_BalancedParenthesis.py
    Added comments to explain logic

--- M_BalancedParenthesis.py
+++ M_BalancedParenthesis.py
@@ -4,10 +4,15 @@ def main():
 	parenthesis=input().split(' ')
 	
 	stack=[]
+	#count represents the count of the current subarray that is going.
 	count=0
+	
+	#Mirror count represents the count of the shortest subarray that is balanced one after the other
+	#Example [2,3,4,-4,-3,-2] will give MirroredCount as 6.
 	MirrorCount=0
+	
 	balancedCount=[]
-	balancedCount.append(0)
+	balancedCount.append(0) #Appending Zero in case there is no balanced array
 	
 	for x in parenthesis:
 		xx = int(x)
@@ -15,16 +20,19 @@ def main():
 			stack.append(xx)
 			count+=1
 			MirrorCount=0
+			
 		elif xx < 0:
 			if(len(stack)>0):
 				y = stack.pop()
 				if((-1*xx) == y):
 					count+=1
+					
+					#As soon as a negative number balances out a positive number, we give mirrored count +2
+					#So the sequence of balanced arrays are added per 2 continuously
 					MirrorCount+=2
+					
 					balancedCount.append(MirrorCount)
-					#print("-1 * xx : {}".format(-1*xx))
-					#print("y : {}".format(y))
-					#print("Count Check After Balance Positive: {}".format(count))
+					
 					if(len(stack)==0):
 						balancedCount.append(count)
 				
